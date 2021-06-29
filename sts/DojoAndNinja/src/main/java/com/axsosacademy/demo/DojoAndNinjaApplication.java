6ea51b04bc94bc6969c718843edb74268dace38a
yousef <ronorozoroz@hotmail.com>
2021-06-29 13:23:54 +0300

    old

---
+++ sts/DojoAndNinja/src/main/java/com/axsosacademy/demo/DojoAndNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.axsosacademy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoAndNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoAndNinjaApplication.class, args);
+	}
+
+}
